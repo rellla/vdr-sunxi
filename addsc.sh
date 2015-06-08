7a4f965b74f037d6cd7fd6f16f965387310bf572
rellla <ichgeh@imkreisrum.de>
2015-06-08 10:13:07 -0400

    Fix creating sc repo conf

---
+++ addsc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+	if [[ $# -eq 1 ]]; then
+	    REPO=$1
+	else
+	    echo "arguments not valid"
+	    echo $#
+	    exit 1
+	fi
+
+	if [ ! -e src/vdr-plugin-sc ]; then
+		echo "vdr-plugin-sc directory missing"
+		exit 1
+	else
+		sed -e s/URL0/${REPO}/g src/vdr-plugin-sc/vdr-plugin-sc.template > src/vdr-plugin-sc/vdr-plugin-sc.conf
+	fi
